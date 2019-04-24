5c09e7053485d714d4380b707e76bdf5c0e23c59
Jérémy RASENDRASON <xblackfieldx@gmail.com>
2019-04-24 09:53:16 +0200

    First commit

---
+++ Assets/Scripts/Pathfinding/AStar/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding{
+    public class Node
+    {
+        public bool IsWalkable;
+        public Vector3 WorldPosition;
+
+        public Node(bool walk, Vector3 wp)
+        {
+            IsWalkable = walk;
+            WorldPosition = wp;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Pathfinding/AStar/PathfindingGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding {
+    public class PathfindingGrid : MonoBehaviour
+    {
+        public Vector2 GridWorldSize;
+        public float NodeRadius;
+        public Node[][] grid;
+        public LayerMask UnwalkableLayer;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector3(GridWorldSize.x, 1, GridWorldSize.y));
+
+        }
+    }
+}
+
